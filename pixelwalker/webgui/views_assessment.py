4fc9de8477b5b9de208ada0c2ddd4a0e307392ea
Antoine Henning <henning.antoine@gmail.com>
2017-08-04 19:38:22 +0200

    add missing imports
    my bad !

--- pixelwalker/webgui/views_assessment.py
+++ pixelwalker/webgui/views_assessment.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 
 # import db models
