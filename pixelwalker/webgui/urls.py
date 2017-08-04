fb08ad7933883fc5b8b18444395c156690efa40b
Antoine Henning <henning.antoine@gmail.com>
2017-08-04 19:22:38 +0200

    add assessment views skeleton in webgui app

--- pixelwalker/webgui/urls.py
+++ pixelwalker/webgui/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
 
 from . import views
+from . import views_assessment
 
 urlpatterns = [
     url(r'^$', views.hello_world, name='index'),
+    #assessment
+    url(r'^assessment/$', views_assessment.list, name='assessment_list'),
+    url(r'^assessment/create/$', views_assessment.create, name='assessment_create'),
+    url(r'^assessment/(?P<assessment_id>[0-9]+)/$', views_assessment.read, name='assessment_read'),
+    url(r'^assessment/(?P<assessment_id>[0-9]+)/update/$', views_assessment.update, name='assessment_update'),
+    url(r'^assessment/(?P<assessment_id>[0-9]+)/delete/$', views_assessment.delete, name='assessment_delete'),
+
 ]
---
+++ pixelwalker/webgui/views_assessment.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# import db models
+from engine.models import EncodingProvider, Media, Assessment
+
+
+# List all assessments
+def list(request):
+    assessment_list = Assessment.objects.all().order_by('name')
+    return render(request, 'assessment/list.html', {'assessment_list':assessment_list})
+
+
+#Crud
+def create(request):
+    # Submitting values for new assessment
+    if request.POST:
+        new_assessment = Assessment()
+        # set values
+        new_assessment.save()
+        return HttpResponseRedirect(reverse('assessment:assessment_read', args=(new_assessment.id,)))
+
+    # Asking for the new assessment form
+    else:
+        return render(request, 'assessment/create.html')
+
+
+#cRud
+def read(request, assessment_id):
+    assessment = get_object_or_404(Assessment, pk=assessment_id)
+    return render(request, 'assessment/read.html', {'assessment': assessment})
+
+
+#crUd
+def update(request, assessment_id):
+    assessment = get_object_or_404(Assessment, pk=assessment_id)
+
+    # Submitting values for updating assessment
+    if request.POST:
+        # update values
+        assessment.save()
+        return HttpResponseRedirect(reverse('assessment:assessment_read', args=(assessment.id,)))
+
+    # Asking for the new assessment form
+    else:
+        return render(request, 'assessment/create.html')
+
+
+#cruD
+def delete(request, assessment_id):
+    assessment = Assessment.objects.filter(id=assessment_id)[0]
+    assessment.delete()
+    return HttpResponseRedirect(reverse('assessment:assessment_list'))
