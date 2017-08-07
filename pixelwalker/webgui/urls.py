9f2e40f802acf222e6983f18e41a6660caa9c633
Antoine Henning <henning.antoine@gmail.com>
2017-08-07 17:51:53 +0200

    Add encoding provider basic webgui integration

--- pixelwalker/webgui/urls.py
+++ pixelwalker/webgui/urls.py
@@ -2,14 +2,20 @@ from django.conf.urls import url
 
 from . import views
 from . import views_assessment
+from . import views_encoding_provider
 
 urlpatterns = [
     url(r'^$', views.hello_world, name='index'),
-    #assessment
+    # Assessment
     url(r'^assessment/$', views_assessment.list, name='assessment_list'),
     url(r'^assessment/create/$', views_assessment.create, name='assessment_create'),
     url(r'^assessment/(?P<assessment_id>[0-9]+)/$', views_assessment.read, name='assessment_read'),
     url(r'^assessment/(?P<assessment_id>[0-9]+)/update/$', views_assessment.update, name='assessment_update'),
     url(r'^assessment/(?P<assessment_id>[0-9]+)/delete/$', views_assessment.delete, name='assessment_delete'),
-
+	# EncodingProvider
+    url(r'^encoding_provider/$', views_encoding_provider.list, name='encoding_provider_list'),
+    url(r'^encoding_provider/create/$', views_encoding_provider.create, name='encoding_provider_create'),
+    url(r'^encoding_provider/(?P<encoding_provider_id>[0-9]+)/$', views_encoding_provider.read, name='encoding_provider_read'),
+    url(r'^encoding_provider/(?P<encoding_provider_id>[0-9]+)/update/$', views_encoding_provider.update, name='encoding_provider_update'),
+    url(r'^encoding_provider/(?P<encoding_provider_id>[0-9]+)/delete/$', views_encoding_provider.delete, name='encoding_provider_delete'),
 ]
---
+++ pixelwalker/webgui/views_encoding_provider.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+
+# import db models
+from engine.models import EncodingProvider, Media, Assessment
+
+
+# List all encoding providers
+def list(request):
+    encoding_provider_list = EncodingProvider.objects.all().order_by('name')
+    return render(request, 'encoding_provider/list.html', {'encoding_provider_list':encoding_provider_list})
+
+
+#Crud
+def create(request):
+    # Submitting values for new encoding provider
+    if request.POST:
+        new_encoding_provider = EncodingProvider()
+        # set values
+        new_encoding_provider.name = request.POST.get('name')
+        new_encoding_provider.save()
+        return HttpResponseRedirect(reverse('webgui:encoding_provider_read', args=(new_encoding_provider.id,)))
+
+    # Asking for the new encoding provider form
+    else:
+        return render(request, 'encoding_provider/create.html')
+
+
+#cRud
+def read(request, encoding_provider_id):
+    encoding_provider = get_object_or_404(EncodingProvider, pk=encoding_provider_id)
+    return render(request, 'encoding_provider/read.html', {'encoding_provider': encoding_provider})
+
+
+#crUd
+def update(request, encoding_provider_id):
+    encoding_provider = get_object_or_404(EncodingProvider, pk=encoding_provider_id)
+
+    # Submitting values for updating encoding provider
+    if request.POST:
+        # update values
+        encoding_provider.name = request.POST.get('name')
+        encoding_provider.save()
+        return HttpResponseRedirect(reverse('webgui:encoding_provider_read', args=(encoding_provider.id,)))
+
+    # Asking for the edit encoding provider form
+    else:
+        return render(request, 'encoding_provider/update.html', {'encoding_provider': encoding_provider})
+
+
+#cruD
+def delete(request, encoding_provider_id):
+    encoding_provider = EncodingProvider.objects.filter(id=encoding_provider_id)[0]
+
+    # If delete confirm
+    if request.POST:
+        # delete the encoding provider object
+        if encoding_provider_id == request.POST.get('delete_id'):
+            encoding_provider.delete()
+            return HttpResponseRedirect(reverse('webgui:encoding_provider_list'))
+        else:
+            return render(request, 'encoding_provider/read.html', {'encoding_provider': encoding_provider})
+
+    # Asking for the delete confirmation form
+    else:
+        return render(request, 'encoding_provider/delete.html', {'encoding_provider': encoding_provider})
