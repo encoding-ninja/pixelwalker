d946d72e029e719eeef000efb7237d80d0d1b971
Antoine Henning <henning.antoine@gmail.com>
2017-12-08 17:19:57 +0100

    clean slate

--- pixelwalker/engine/utils.py
+++
@@ -1,9 +0,0 @@
-
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-import os
-import uuid
-
-def get_upload_path(instance, filename):
-    return os.path.join(filename+"_"+str(uuid.uuid4().hex), filename)
--- pixelwalker/webgui/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def hello_world(request):
-    return HttpResponse("Hello, world.")
