8595351fbe51a9d0824d399a4edd6c927b43ab22
Antoine Henning <henning.antoine@gmail.com>
2017-12-11 23:09:27 +0100

    assessment basic views

--- pixelwalker/engine/urls.py
+++ pixelwalker/engine/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
 	# API v1
     path('api/v1/', api.index, name='index'),
     # WEBGUI
-    path('', assessment.list, name='assessment_list'),   
+    path('', assessment.list, name='webgui_assessment-list'),   
+    path('assessment/create', assessment.create, name='webgui_assessment-create'),   
+    path('assessment/<int:assessment_id>', assessment.read, name='webgui_assessment-read'),   
+    path('assessment/<int:assessment_id>/update', assessment.update, name='webgui_assessment-update'),   
+    path('assessment/<int:assessment_id>/delete', assessment.delete, name='webgui_assessment-delete'),   
 ]
--- pixelwalker/pixelwalker/urls.py
+++ pixelwalker/pixelwalker/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 #from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('engine.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
