b6226a9a73bf2f1b76ee6814aba034005901fad5
Antoine Henning <henning.antoine@gmail.com>
2017-12-11 19:27:17 +0100

    clean install bower dependencies

--- pixelwalker/pixelwalker/settings.py
+++ pixelwalker/pixelwalker/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'pixelwalker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "engine", "webgui", "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
