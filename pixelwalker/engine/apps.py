984c4f5bd4ad5549c5e0212a9e6923c95b07fa5b
antoinehng <antoine@macbookpro.home>
2018-01-20 19:46:16 +0100

    building the pulling task service

--- pixelwalker/engine/apps.py
+++ pixelwalker/engine/apps.py
@@ -1,21 +1,12 @@
 from django.apps import AppConfig
-from .service import Service
-import sched, time
-
-
 
 class EngineConfig(AppConfig):
     name = 'engine'
-    task_service = Service()
     
     def ready(self):
         print("App ready!")
+        from .service import Service
+        self.task_service = Service()
+        
         if not self.task_service.running:
             self.task_service.start()
-        
-   
-        
-        
-
-
-        
