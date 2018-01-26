586d84df449a690a781e532deb2402f7a73e1c93
antoinehng <antoine@macbookpro.home>
2018-01-26 21:55:46 +0100

    comment pulling service launch
    to be replaced by Celery tasks

--- pixelwalker/engine/apps.py
+++ pixelwalker/engine/apps.py
@@ -4,9 +4,10 @@ class EngineConfig(AppConfig):
     name = 'engine'
     
     def ready(self):
-        print("App ready!")
-        from .service import Service
-        self.task_service = Service()
+        pass
+        #print("App ready!")
+        #from .service import Service
+        #self.task_service = Service()
         
-        if not self.task_service.running:
-            self.task_service.start()
+        #if not self.task_service.running:
+        #    self.task_service.start()
