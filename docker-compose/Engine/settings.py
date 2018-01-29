84ebd15bede36e19960efdcf1e7c1004895b6569
antoinehng <henning.antoine@gmail.com>
2018-01-29 16:08:27 +0100

    fixes docker-compose services launch
    still need broker set-up

--- docker-compose/Engine/settings.py
+++ docker-compose/Engine/settings.py
@@ -77,15 +77,14 @@ WSGI_APPLICATION = 'pixelwalker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {  
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_ENV_DB', 'postgres'),
-        'USER': os.environ.get('DB_ENV_POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_ENV_POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_PORT_5432_TCP_ADDR', 'db'),
-        'PORT': os.environ.get('DB_PORT_5432_TCP_PORT', 5432),
-    },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
 }
 
 
@@ -143,57 +142,14 @@ REDIS_PORT = 6379
 REDIS_DB = 0  
 REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', 'redis')
 
-RABBIT_HOSTNAME = os.environ.get('RABBIT_PORT_5672_TCP', 'rabbit')
 
-if RABBIT_HOSTNAME.startswith('tcp://'):  
-    RABBIT_HOSTNAME = RABBIT_HOSTNAME.split('//')[1]
+# Celery settings
 
-BROKER_URL = os.environ.get('BROKER_URL',  
-                            '')
-if not BROKER_URL:  
-    BROKER_URL = 'amqp://{user}:{password}@{hostname}/{vhost}/'.format(
-        user=os.environ.get('RABBIT_ENV_USER', 'admin'),
-        password=os.environ.get('RABBIT_ENV_RABBITMQ_PASS', 'mypass'),
-        hostname=RABBIT_HOSTNAME,
-        vhost=os.environ.get('RABBIT_ENV_VHOST', ''))
+CELERY_BROKER_URL = 'amqp://admin:mypass@localhost//'
 
-# We don't want to have dead connections stored on rabbitmq, so we have to negotiate using heartbeats
-BROKER_HEARTBEAT = '?heartbeat=30'  
-if not BROKER_URL.endswith(BROKER_HEARTBEAT):  
-    BROKER_URL += BROKER_HEARTBEAT
-
-BROKER_POOL_LIMIT = 1  
-BROKER_CONNECTION_TIMEOUT = 10
-
-# Celery configuration
-
-# configure queues, currently we have only one
-CELERY_DEFAULT_QUEUE = 'default'  
-CELERY_QUEUES = (  
-    Queue('default', Exchange('default'), routing_key='default'),
-)
-
-# Sensible settings for celery
-CELERY_ALWAYS_EAGER = False  
-CELERY_ACKS_LATE = True  
-CELERY_TASK_PUBLISH_RETRY = True  
-CELERY_DISABLE_RATE_LIMITS = False
-
-# By default we will ignore result
-# If you want to see results and try out tasks interactively, change it to False
-# Or change this setting on tasks level
-CELERY_IGNORE_RESULT = True  
-CELERY_SEND_TASK_ERROR_EMAILS = False  
-CELERY_TASK_RESULT_EXPIRES = 600
-
-# Set redis as celery result backend
-CELERY_RESULT_BACKEND = 'redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB)  
+#: Only add pickle to this list if your broker is secured
+#: from unwanted access (see userguide/security.html)
+CELERY_ACCEPT_CONTENT = ['json', 'application/json']
+CELERY_RESULT_BACKEND = 'redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB) 
 CELERY_REDIS_MAX_CONNECTIONS = 1
-
-# Don't use pickle as serializer, json is much safer
-CELERY_TASK_SERIALIZER = "json"  
-CELERY_ACCEPT_CONTENT = ['application/json']
-
-CELERYD_HIJACK_ROOT_LOGGER = False  
-CELERYD_PREFETCH_MULTIPLIER = 1  
-CELERYD_MAX_TASKS_PER_CHILD = 1000  
+CELERY_TASK_SERIALIZER = 'json'
