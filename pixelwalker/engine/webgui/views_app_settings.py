20874efe3ba2dc89cce4e813c0e57ba96b070804
antoinehng <antoine@macbookpro.local>
2018-02-01 18:22:27 +0100

    add auto submit task type settings
    Have to add a way to manually submit

--- pixelwalker/engine/webgui/views_app_settings.py
+++ pixelwalker/engine/webgui/views_app_settings.py
@@ -6,27 +6,34 @@ from django.views import generic
 
 
 # import db models
-from ..models import AppSettings
+from ..models import AppSettings, TaskType
 
 
 #cRud
 def read(request):
     app_settings = get_object_or_404(AppSettings, pk=1)
-    return render(request, 'app_settings/read.html', {'app_settings': app_settings})
+    metric_list = TaskType.objects.filter(is_video_metric=False).order_by('name')
+    return render(request, 'app_settings/read.html', {'app_settings': app_settings, 'metric_list': metric_list})
 
 #crUd
 def update(request):
     app_settings = get_object_or_404(AppSettings, pk=1)
+    metric_list = TaskType.objects.filter(is_video_metric=False).order_by('name')
 
     # Submitting values for updating encoding provider
     if request.POST:
-        # update values
-        app_settings.max_parallel_tasks = request.POST.get('max_parallel_tasks')
-        app_settings.worker_pulling_interval = request.POST.get('worker_pulling_interval')
-        app_settings.save()
+        
+        # update task type values
+        for metric in metric_list:
+            if request.POST.get(str(metric.id)+'_auto_submit_on_new_media') is not None:
+                metric.auto_submit_on_new_media = True
+            else:
+                metric.auto_submit_on_new_media = False
+            metric.save()
+            
         return HttpResponseRedirect(reverse('webgui_app-settings-read'))
 
     # Asking for the edit encoding provider form
     else:
-        return render(request, 'app_settings/update.html', {'app_settings': app_settings})
+        return render(request, 'app_settings/update.html', {'app_settings': app_settings, 'metric_list': metric_list})
 
