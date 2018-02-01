95ee73c4e5af50631c06206b99824e4498b39cc1
antoinehng <antoine@macbookpro.local>
2018-02-01 19:03:47 +0100

    highlight best and worst encoded variants

--- pixelwalker/engine/templatetags/utils.py
+++ pixelwalker/engine/templatetags/utils.py
@@ -12,3 +12,19 @@ def assessment_media_tasktype_exists(assessment, media, tasktype):
 def media_tasktype_exists(media, tasktype):
     task = Task.objects.filter(media=media, type=tasktype)
     return task
+
+@register.simple_tag
+def is_best_assessment_task(assessment, task):
+    assessment_task_list = Task.objects.filter(assessment=assessment, type=task.type)
+    if task.get_average_score() == max(t.get_average_score() for t in assessment_task_list):
+        return True
+    else:
+        return False
+
+@register.simple_tag
+def is_worst_assessment_task(assessment, task):
+    assessment_task_list = Task.objects.filter(assessment=assessment, type=task.type)
+    if task.get_average_score() == min(t.get_average_score() for t in assessment_task_list):
+        return True
+    else:
+        return False
